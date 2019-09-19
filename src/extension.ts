3fd5579f2f5aa689adc2831538e4315e3cccf980
gxx <xx.gui@ctrip.com>
2019-09-19 15:12:22 +0800

    init

---
+++ src/extension.ts
@@ -0,0 +1,40 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "cleartime" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello1 World!');
+	// });
+	context.subscriptions.push(vscode.commands.registerCommand('extension.helloWorld', function (uri) {
+		// 创建webview
+		const panel = vscode.window.createWebviewPanel(
+			'testWebview', // viewType
+			"WebView演示", // 视图标题
+			vscode.ViewColumn.One, // 显示在编辑器的哪个部位
+			{
+				enableScripts: true, // 启用JS，默认禁用
+				retainContextWhenHidden: true, // webview被隐藏时保持状态，避免被重置
+			}
+		);
+		panel.webview.html = `<html><body>你好，我是cleartime-Webview</body></html>`
+	}))
+	// context.subscriptions.push(disposable);
+}
+
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
